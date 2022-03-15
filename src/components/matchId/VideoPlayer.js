45d4af9c800b12def9aaab8b0b8f8cc91dae4cd2
gholib <gtrandnet@gmail.com>
2022-03-16 01:37:42 +0500

    f

--- src/components/matchId/VideoPlayer.js
+++ src/components/matchId/VideoPlayer.js
@@ -9,12 +9,12 @@ export default function VideoPlayer({ liveStart }) {
   return (
     <div className="player_video">
       <div className="pl__wrapper">
-          {/* <ReactPlayer
+          <ReactPlayer
             width={"100%"}
-            url="https://matchtv.ru/vdl/player/media/608733"
-        /> */}
+            url="blob:https://matchtv.ru/30fa3ed2-8372-4871-9e4d-58268297feb6"
+        />
       </div>
-      <div><br />		<iframe allowfullscreen="true" scrolling="no" frameborder="0" width="100%" height="580" src="https://matchtv.ru/vdl/player/media/608733"></iframe></div>
+      {/* <div><br />		<iframe allowfullscreen="true" scrolling="no" frameborder="0" width="100%" height="580" src="https://matchtv.ru/vdl/player/media/608733"></iframe></div> */}
     </div>
   );
 }
