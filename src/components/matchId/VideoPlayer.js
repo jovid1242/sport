1fd0429d299525eb36561e9d0d3f19df0a044e2f
gholib <gtrandnet@gmail.com>
2022-03-16 01:25:19 +0500

    fix

--- src/components/matchId/VideoPlayer.js
+++ src/components/matchId/VideoPlayer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactPlayer from "react-player/youtube";
+import ReactPlayer from "react-player";
 
 import domatch from "../../assets/img/domatch.png";
 
@@ -8,19 +8,13 @@ import "../../styles/player.scss";
 export default function VideoPlayer({ liveStart }) {
   return (
     <div className="player_video">
-      {/* <div className="pl__wrapper">
-        {liveStart === false ? (
-          <div className="img_start_match">
-            <img src={domatch} alt="" />
-          </div>
-        ) : (
-          <ReactPlayer
+      <div className="pl__wrapper">
+          {/* <ReactPlayer
             width={"100%"}
-            url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
-          />
-        )}
-      </div> */}
-      <div><br />		<iframe allowfullscreen="true" scrolling="no" frameborder="0" width="100%" height="580" src="http://var16.ru/livetv/live/4.html"></iframe></div>
+            url="https://matchtv.ru/vdl/player/media/608733"
+        /> */}
+      </div>
+      <div><br />		<iframe allowfullscreen="true" scrolling="no" frameborder="0" width="100%" height="580" src="https://matchtv.ru/vdl/player/media/608733"></iframe></div>
     </div>
   );
 }
