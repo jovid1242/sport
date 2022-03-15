03fe194c5eee2da67a0c0e9cccde36d049573006
gholib <gtrandnet@gmail.com>
2022-03-16 00:55:39 +0500

    fix

--- src/components/matchId/VideoPlayer.js
+++ src/components/matchId/VideoPlayer.js
@@ -8,7 +8,7 @@ import "../../styles/player.scss";
 export default function VideoPlayer({ liveStart }) {
   return (
     <div className="player_video">
-      <div className="pl__wrapper">
+      {/* <div className="pl__wrapper">
         {liveStart === false ? (
           <div className="img_start_match">
             <img src={domatch} alt="" />
@@ -19,7 +19,8 @@ export default function VideoPlayer({ liveStart }) {
             url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
           />
         )}
-      </div>
+      </div> */}
+      <div><br/>		<iframe allowfullscreen="true" scrolling="no" frameborder="0" width="100%" height="580" src="http://var16.ru//4.html"></iframe></div>
     </div>
   );
 }
